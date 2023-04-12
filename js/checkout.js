02dfd622cd1116191ed008e34c33c5c907d2e83e
Dar-ya218 <dasharoura13465@gmail.com>
2023-04-13 00:31:15 +0200

    Ejercicio_validate

--- js/checkout.js
+++ js/checkout.js
@@ -1,75 +1,21 @@
 // Exercise 6(Validate)
 
-function validate() {
-		let error = 0;
-		let checkoutForm = document.getElementById("checkoutForm");
-		
-		// Get the input fields
-		let fName = document.getElementById("fName");
-		let fEmail = document.getElementById("fEmail");
-		let fAddress = document.getElementById("fAddress");
-		let fLastN = document.getElementById("fLastN");
-		let fPassword = document.getElementById("fPassword");
-		let fPhone = document.getElementById("fPhone");  
-		
-		// Validate fields entered by the user: name, phone, password, and email
-		if( !fName.value.match(/^[A-Za-z\s]*$/) || fName.value.length < 3 ){
-			applyInvalidClassList(fName,true)
-			error++;
-		} else {
-			applyInvalidClassList(fName, false)
+	// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+	'use strict'
+  
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	const forms = document.querySelectorAll('.needs-validation')
+  
+	// Loop over them and prevent submission
+	Array.from(forms).forEach(form => {
+	  form.addEventListener('submit', event => {
+		if (!form.checkValidity()) {
+		  event.preventDefault()
+		  event.stopPropagation()
 		}
-	
-		if( !fEmail.value.match(
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) 
-			|| fEmail.value.length < 3 ){
-			applyInvalidClassList(fEmail,true)
-			error++;
-		} else {
-			applyInvalidClassList(fEmail,false)
-		}
-	
-		if( fAddress.value.length < 3 ){
-			applyInvalidClassList(fAddress,true)
-			error++;
-		} else {
-			applyInvalidClassList(fAddress, false)
-		}
-	
-		if( !fLastN.value.match(/^[A-Za-z\s]*$/) || fLastN.value.length < 3 ){
-			applyInvalidClassList(fLastN, true)
-			error++
-		} else {
-			applyInvalidClassList(fLastN,false)
-		}
-	
-		if( isNaN(fPhone.value) || fPhone.value.length !== 9 ){
-			applyInvalidClassList(fPhone, true)
-			error++
-		} else {
-			applyInvalidClassList(fPhone,false)
-		}
-	
-		if( !fPassword.value.match(/^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{3,8}$/) ||
-			fPassword.value.length < 3 ){
-				applyInvalidClassList(fPassword, true)
-			error++
-		} else {
-			applyInvalidClassList(fPassword, false)
-		}
-	
-		if ( error > 0 ) {
-			checkoutForm.addEventListener("submit",(e) => {e.preventDefault(); },);
-		} else {
-			alert("Form submitted successfully");
-		}
-	}
-	
-	function applyInvalidClassList(element, error) {
-		if ( error ) {
-		  element.classList.add("is-invalid");
-		} else {
-		  element.classList.remove("is-invalid");
-		  element.classList.add("is-valid");
-		}
-	}
+  
+		form.classList.add('was-validated')
+	  }, false)
+	})
+  })()
